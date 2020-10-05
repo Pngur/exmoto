5f5780f66188b4d3b5c5433d83cb154d8a5c30ba
pngur <apangur@yandex.ru>
2020-10-05 15:41:28 +0300

    Updates

--- precache-manifest.ab50f892e32f061bfe5fd2a1135004e2.js
+++ precache-manifest.ab50f892e32f061bfe5fd2a1135004e2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58b97ee87d7e06e55a57ef5e94a55aad",
+    "revision": "fc37c38953a783acece83a415f529659",
     "url": "/exmoto/index.html"
   },
   {
-    "revision": "9208cb7057d20386c683",
+    "revision": "7cdea7b8803afa797d8f",
     "url": "/exmoto/static/css/main.f2291842.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/exmoto/static/js/2.31682806.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9208cb7057d20386c683",
-    "url": "/exmoto/static/js/main.d3b43219.chunk.js"
+    "revision": "7cdea7b8803afa797d8f",
+    "url": "/exmoto/static/js/main.034a7f91.chunk.js"
   },
   {
     "revision": "562b7bd1c3b0d8952eef",
