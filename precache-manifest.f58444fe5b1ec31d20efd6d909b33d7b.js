c64b8fa7589e2e1001bfef178eb0151fff42a0c6
pngur <apangur@yandex.ru>
2020-10-05 15:22:01 +0300

    Updates

---
+++ precache-manifest.f58444fe5b1ec31d20efd6d909b33d7b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56d9e183d527094c11460b87593b26b",
+    "url": "/exmoto/index.html"
+  },
+  {
+    "revision": "9a00536eb1e8cc3cc85d",
+    "url": "/exmoto/static/css/main.f2291842.chunk.css"
+  },
+  {
+    "revision": "3d926bd989b4b3745351",
+    "url": "/exmoto/static/js/2.31682806.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/exmoto/static/js/2.31682806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a00536eb1e8cc3cc85d",
+    "url": "/exmoto/static/js/main.24351e68.chunk.js"
+  },
+  {
+    "revision": "562b7bd1c3b0d8952eef",
+    "url": "/exmoto/static/js/runtime-main.196dadfb.js"
+  },
+  {
+    "revision": "39db7eb12c7a213884b07f8fcdee3e9f",
+    "url": "/exmoto/static/media/b2b.39db7eb1.jpg"
+  },
+  {
+    "revision": "0e601bd8c9ae25a045ffea8cb0f1ca5b",
+    "url": "/exmoto/static/media/box.0e601bd8.jpg"
+  },
+  {
+    "revision": "da188ccb78f1017e9645542bb5e52d6b",
+    "url": "/exmoto/static/media/checked.da188ccb.png"
+  },
+  {
+    "revision": "a5230cd6c333c579417d05e98cb2f3e7",
+    "url": "/exmoto/static/media/main-logo.a5230cd6.png"
+  },
+  {
+    "revision": "9e2e6db0028eb736e73b0d00456c7ed9",
+    "url": "/exmoto/static/media/truck.9e2e6db0.jpg"
+  }
+]);
